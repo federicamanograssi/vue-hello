961dbeb331a483b766d5e81a106fbc2f29cf686c
Federica <federica.manograssi@gmail.com>
2021-03-18 15:25:43 +0100

    Es 01 ok

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+var myVue = new Vue(
+    {
+        el : '#myVue',
+        data : {
+            myTitle : 'Com\'è umano lei, oggi'
+        }
+    }
+
+    // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+    
+);
+
+
+
