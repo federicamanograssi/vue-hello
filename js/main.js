c52ba2195b06c1a7c132f64a186f1e7bee95fbe5
Federica <federica.manograssi@gmail.com>
2021-03-18 15:49:15 +0100

    Img BONUS inserita

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ var myVue = new Vue(
     {
         el : '#myVue',
         data : {
-            myTitle : 'Com\'è umano lei, oggi'
+            myTitle : 'Com\'è umano lei, oggi',
+            
+            // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+            
+            myImg : 'https://dissidenzaquotidiana.it/wp-content/uploads/2020/07/fantozzi-paolo-villaggio.jpg'
         }
     }
 
-    // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-    
 );
 
 
